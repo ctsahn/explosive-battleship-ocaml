0f5c05d1c72dd5bc9d7003ee04741265f599bb2b
Julia <julia.bian92@gmail.com>
2022-11-29 00:07:46 -0500

    fixed board

--- src/board.ml
+++ src/board.ml
@@ -1,23 +1,24 @@
+open Core
+
 type status = Empty | Miss | Ship | ShipHit | ShipSunken
 
 type t = status array array
 
 let board_to_string (board: t) : string = 
-  let l = List.concat @@ (List.map Array.to_list (Array.to_list board)) in 
+  let l = List.concat @@ (List.map ~f:Array.to_list (Array.to_list board)) in 
   let converted = 
-    List.map 
-      (fun ele ->
+    List.map l
+      ~f:(fun ele ->
         match ele with 
         | Empty -> "0"
         | Miss -> "1"
         | Ship -> "2"
         | ShipHit -> "3" 
         | ShipSunken -> "4")
-      l
   in 
-  String.concat "" converted
+  String.concat ~sep:"" converted
 
 let initialize_board : t =
-  Array.make_matrix 10 10 Empty
+  Array.make_matrix ~dimx:10 ~dimy:10 Empty
 
 let convert_position (pos: int) : int * int = (pos mod 10, pos / 10)
