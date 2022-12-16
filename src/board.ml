39876161234043eb9bbc113c5361eccab2769921
Julia <julia.bian92@gmail.com>
2022-12-16 15:57:10 -0500

    dune

--- src/board.ml
+++ src/board.ml
@@ -24,21 +24,21 @@ let populate_board (board : t) (input_str : string) : unit =
   let board_len = Array.length board in
 
   Array.iteri board ~f:(fun i _ ->
-      let num_list =
-        String.subo ~pos:(i * board_len) ~len:board_len input_str
-        |> String.to_list
-      in
+    let num_list =
+      String.subo ~pos:(i * board_len) ~len:board_len input_str
+      |> String.to_list
+    in
 
-      List.iteri num_list ~f:(fun j el ->
-        match el with
-        | '0' -> board.(i).(j) <- Empty
-        | '1' -> board.(i).(j) <- Miss
-        | '2' -> board.(i).(j) <- Ship
-        | '3' -> board.(i).(j) <- ShipHit
-        | '4' -> board.(i).(j) <- ShipSunken
-        | '5' -> board.(i).(j) <- Mine
-        | '6' -> board.(i).(j) <- MineHit
-        | _ -> failwith "None"))
+    List.iteri num_list ~f:(fun j el ->
+      match el with
+      | '0' -> board.(i).(j) <- Empty
+      | '1' -> board.(i).(j) <- Miss
+      | '2' -> board.(i).(j) <- Ship
+      | '3' -> board.(i).(j) <- ShipHit
+      | '4' -> board.(i).(j) <- ShipSunken
+      | '5' -> board.(i).(j) <- Mine
+      | '6' -> board.(i).(j) <- MineHit
+      | _ -> failwith "None"))
 
 let initialize_boards : t * t =
   ( Array.make_matrix ~dimx:10 ~dimy:10 Empty,
